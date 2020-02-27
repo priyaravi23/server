d2f7d9f345c9bdb73a9470e7cf3a8efee500ffbc
Priya Ravi <priyaravi23@gmail.com>
2020-02-27 11:53:22 -0800

    added todo-cypress

---
+++ public/precache-manifest.2245aee0ea51417f136063b78388bfdc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca621e820afe3c0612466c5bd809f289",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11bbc410cabd72a47c08",
+    "url": "/static/css/main.5c49816e.chunk.css"
+  },
+  {
+    "revision": "3902f19400fc507b6b4f",
+    "url": "/static/js/2.f64c39bf.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.f64c39bf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "11bbc410cabd72a47c08",
+    "url": "/static/js/main.73908581.chunk.js"
+  },
+  {
+    "revision": "96acfca3d26b469cc53a",
+    "url": "/static/js/runtime-main.13e6840c.js"
+  }
+]);
