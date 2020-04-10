45c89774609b4ac5cedc128b205fd608063a03a1
Priya Ravi <priyaravi23@gmail.com>
2020-04-09 19:04:40 -0700

    fixed issue with width rental app

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.11bb8ef2d5477b861f76d88ccee45765.js"
+  "/precache-manifest.3e9807cb2101e8cf63417c035c352d5b.js"
 );
 
 self.addEventListener('message', (event) => {
